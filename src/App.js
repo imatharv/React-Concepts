494d1e3aac23365287abd1b779c88721884ca163
Atharva Joshi <iamatharvaj@gmail.com>
2021-07-28 10:57:45 +0530

    Added useState with object

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from "./pages/HomePage"
 import DetailsPage from "./pages/DetailsPage"
 import CallBackAndUseMemoComponent from './components/CallBackAndUseMemoComponent';
+import UseStateWithObjects from './components/UseStateWithObjects'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route exact path = "/dashboard" component = {HomePage} />
           <Route exact path = "/details" component = {DetailsPage} />
           <Route exact path = "/callback&memo" component = {CallBackAndUseMemoComponent} />
+          <Route exact path = "/others" component = {UseStateWithObjects} />
         </Switch>
       </BrowserRouter>
   );
---
+++ src/components/UseStateWithObjects.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function UseStateWithObjects() {
+  const [details, setDetails] = React.useState({ name: "", city: "" });
+  return (
+    <div className="container mt-5 pt-5">
+      <div className="col justify-content-start mb-4">
+        <label className="lead font-weight-normal" for="name">
+          Name
+        </label>
+        <input
+          className="form-control"
+          id="name"
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setDetails({ ...details, name: e.target.value })}
+        />
+      </div>
+      <div className="col justify-content-start mb-5">
+        <label className="lead font-weight-normal" for="city">
+          City
+        </label>
+        <input
+          className="form-control"
+          id="city"
+          type="text"
+          placeholder="City"
+          onChange={(e) => setDetails({ ...details, city: e.target.value })}
+        />
+      </div>
+      <div className="col justify-content-start">
+        <p className="lead font-weight-normal text-primary">
+          Name: {details.name}
+        </p>
+        <p className="lead font-weight-normal text-primary">
+          City: {details.city}
+        </p>
+      </div>
+    </div>
+  );
+}
